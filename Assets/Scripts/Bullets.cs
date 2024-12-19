085189b4dc94aaa9db6d7b9c4212627414988929
aleole2004 <aleole@alu.edu.gva.es>
2024-12-19 10:43:47 +0100

    Script_Bullets
    First Half

---
+++ Assets/Scripts/Bullets.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Bullets : MonoBehaviour
+{
+    [SerializeField] Transform avionParaSeguir;
+    [SerializeField] GameObject instBullet;
+    [SerializeField] float speed;
+    [SerializeField] float yMax;
+    Vector3 avionPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        instBullet.transform.position += new Vector3 (0,1,0) * speed * Time.deltaTime;
+        
+    }
+    public void OnCreateBullets ()
+    {
+        avionPos = avionParaSeguir.position;
+        Instantiate(instBullet, avionPos, Quaternion.identity);
+    }
+}
